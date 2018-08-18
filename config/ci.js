bd6cdcfd3b6f344f35588fbe7bc4d4426e25e9a8
Ibrahim Ahmed <ahmedolaibrahim@gmail.com>
2018-08-18 10:48:57 +0100

    readme added

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://blogdev:blogdev7890@ds263759.mlab.com:63759/blog_dev',
+  mongoURI: 'mongodb://127.0.0.1:27071/blog_ci',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379'
 };
